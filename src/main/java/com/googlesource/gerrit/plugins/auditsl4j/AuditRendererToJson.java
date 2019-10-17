9e7c84ade4ce3eccc308d8a24d7e637259a2762a
David Pursehouse <dpursehouse@collab.net>
2019-10-17 16:05:56 +0900

    Rename reviewdb package to entities
    Change-Id: Iac33980a612b22b4fbef20b7bcb016626c7b5486

--- src/main/java/com/googlesource/gerrit/plugins/auditsl4j/AuditRendererToJson.java
+++ src/main/java/com/googlesource/gerrit/plugins/auditsl4j/AuditRendererToJson.java
@@ -15,8 +15,8 @@
 package com.googlesource.gerrit.plugins.auditsl4j;
 
 import com.google.common.collect.ListMultimap;
-import com.google.gerrit.json.OutputFormat;
 import com.google.gerrit.entities.Account;
+import com.google.gerrit.json.OutputFormat;
 import com.google.gerrit.server.AccessPath;
 import com.google.gerrit.server.AuditEvent;
 import com.google.gerrit.server.CurrentUser;
